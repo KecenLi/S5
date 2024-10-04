bec5ea2f7b0970c40052c599ef9e477da8e4f9d9
KecenLi <yo22285@bristol.ac.uk>
2024-10-04 17:15:42 +0100

    Completed Task 2 - Producer-Consumer problem solution

--- pc/pc.go
+++ pc/pc.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	//"github.com/ChrisGora/semaphore"
+	"github.com/ChrisGora/semaphore"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -34,30 +35,40 @@ func (buffer *buffer) put(x int) {
 	buffer.write = (buffer.write + 1) % len(buffer.b)
 }
 
-func producer(buffer *buffer, start, delta int) {
+func producer(buffer *buffer, spaceAvailable, workAvailable semaphore.Semaphore, mutex *sync.Mutex, start, delta int) {
 	x := start
 	for {
-		buffer.put(x)
+		spaceAvailable.Wait() // 等待有空位喵~
+		mutex.Lock()          // 加锁保护共享资源喵~
+		buffer.put(x)         // 生产数据喵~
+		mutex.Unlock()        // 解锁喵~
+		workAvailable.Post()  // 通知消费者有新数据了喵~
 		x = x + delta
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 	}
 }
 
-func consumer(buffer *buffer) {
+func consumer(buffer *buffer, spaceAvailable, workAvailable semaphore.Semaphore, mutex *sync.Mutex) {
 	for {
-		_ = buffer.get()
+		workAvailable.Wait()  // 等待有数据可用喵~
+		mutex.Lock()          // 加锁保护共享资源喵~
+		_ = buffer.get()      // 消费数据喵~
+		mutex.Unlock()        // 解锁喵~
+		spaceAvailable.Post() // 通知生产者有空位了喵~
 		time.Sleep(time.Duration(rand.Intn(5000)) * time.Millisecond)
 	}
 }
 
 func main() {
-	buffer := newBuffer(5)
+	buffer := newBuffer(5) // 保持不变，返回的是值类型喵~
+	mutex := &sync.Mutex{} // 初始化互斥锁喵~
 
-	//spaceAvailable := semaphore.Init(5, 5)
-	//workAvailable := semaphore.Init(5, 0)
+	spaceAvailable := semaphore.Init(5, 5) // 初始化空闲空间信号量，开始时有 5 个空位喵~
+	workAvailable := semaphore.Init(5, 0)  // 初始化工作信号量，开始时没有可用数据喵~
 
-	go producer(&buffer, 1, 1)
-	go producer(&buffer, 1000, -1)
+	// 传递缓冲区的地址给 producer 和 consumer 喵~
+	go producer(&buffer, spaceAvailable, workAvailable, mutex, 1, 1)
+	go producer(&buffer, spaceAvailable, workAvailable, mutex, 1000, -1)
 
-	consumer(&buffer)
+	consumer(&buffer, spaceAvailable, workAvailable, mutex)
 }
--- sum/sum.go
+++ sum/sum.go
@@ -8,10 +8,14 @@ import (
 func main() {
 	sum := 0
 	var wg sync.WaitGroup
+	var mu sync.Mutex
+
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
+			mu.Lock() // 加锁喵
 			sum = sum + 1
+			mu.Unlock() // 解锁喵
 			wg.Done()
 		}()
 	}
